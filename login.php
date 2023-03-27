9b9d58ca40405950703192474c49752c5ab2d815
Chayani Suci <chayanisuci205@gmail.com>
2023-03-27 12:37:06 +0700

    new login user

--- login.php
+++ login.php
@@ -40,55 +40,73 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="assets\css\login\login.css" />
+  <!-- <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="assets\css\login\login.css" /> -->
   <!-- Favicons -->
   <link href="img/logo.png" rel="icon">
   <title>Login Mahasiswa</title>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+
+  <!-- css -->
+  <link href="login.css" rel="stylesheet">
 </head>
 
 <body>
-  <div class="container">
-    <div class="forms-container">
-      <div class="signin-signup">
 
-        <form method="post">
-          <h2 class="title">Login</h2>
+  <style>
+    body {
+      background-image: url('img/background.png');
+      background-size: cover;
+
+    }
+  </style>
+
+  <div class="container">
+    <div class="row align-items-center justify-content-center vh-100">
+      <div class="col-md-7">
+        <img src="img/y.png" class="w-100" ;>
+      </div>
+      <div class="col-md-5">
+        <form class="register-form" method="post">
           <?php
           // Display any error messages
           if (isset($error)) {
             echo '<p>' . $error . '</p>';
           }
           ?>
-          <div class="input-field">
-            <i class="fas fa-user"></i>
-            <input type="text" placeholder="NRP" name="tnrp" />
+          <div class="p-5 text-center">
+            <h4>Selamat Datang</h4>
+            <p>Masukkan NRP dan Password Anda</p>
           </div>
-          <div class="input-field">
-            <i class="fas fa-lock"></i>
-            <input type="password" placeholder="Password" name="tpass" />
+          <div class="mb-3">
+            <label for="text" class="form-label">Username</label>
+            <input type="text" class="form-control" name="tnrp" placeholder="Masukkan Username">
           </div>
-          <input type="submit" name="submit" value="Login" class="btn solid" />
+          <div class="mb-4">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" name="tpass" placeholder="Password">
+          </div>
+
+          <div class="mb-3 py-4">
+            <button type="submit" class="btn btn-primary w-100">Masuk</button>
+          </div>
 
         </form>
       </div>
     </div>
-
-    <div class="panels-container">
-      <div class="panel left-panel">
-        <img src="img/mahasiswi.png" class="image" alt="" />
-      </div>
-      <div class="panel right-panel">
-        <div class="content">
-          <button class="btn transparent" id="sign-in-btn">
-            Sign in
-          </button>
-        </div>
-      </div>
-    </div>
   </div>
 
-  <script src="assets\css\login\app.js"></script>
+  <script src="assets/css/login/app.js"></script>
 </body>
 
 </html>
