4b9ac102d885146629a3e48500aa12d2d3170bbc
Chayani Suci <chayanisuci205@gmail.com>
2023-06-29 20:24:13 +0700

    edit pengembalian

--- admin/pengembalian.php
+++ admin/pengembalian.php
@@ -14,58 +14,60 @@ $kon = new Koneksi();
 <?php include_once "template/header.php" ?>
 
 <body>
-  <?php include_once "template/sidebar.php" ?>
-  <div id="main">
-    <header class="mb-3">
-      <a href="#" class="burger-btn d-block d-xl-none">
-        <i class="bi bi-justify fs-3"></i>
-      </a>
-    </header>
+    <?php include_once "template/sidebar.php" ?>
+    <div id="main">
+        <header class="mb-3">
+            <a href="#" class="burger-btn d-block d-xl-none">
+                <i class="bi bi-justify fs-3"></i>
+            </a>
+        </header>
 
-    <div class="page-heading">
-      <div class="page-title">
-        <div class="row">
-          <div class="col-12 col-md-6 order-md-1 order-last">
-            <h3>Pengembalian Peminjaman</h3>
-            <p class="text-subtitle text-muted">A sortable, searchable, paginated table without dependencies thanks to simple-datatables</p>
-          </div>
-          <div class="col-12 col-md-6 order-md-2 order-first">
-            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Pengembalian</li>
-              </ol>
-            </nav>
-          </div>
-        </div>
-      </div>
-      <section class="section">
-        <div class="card">
-          <div class="card-header">
-            Data Pengembalian
-          </div>
-          <div>
-            <form action="" method="POST">
-              <input type="text" name="ktm" placeholder="Tap RFID KTM">
-              <p>Otomatis Terisi Saat Tap RFID</p>
-              <input type="submit" name="cari" value="Cari Data">
-            </form>
-          </div>
-          <div class="card-header">
+        <div class="page-heading">
+            <div class="page-title">
+                <div class="row">
+                    <div class="col-12 col-md-6 order-md-1 order-last">
+                        <h3>Pengembalian Peminjaman</h3>
+                        <p class="text-subtitle text-muted">A sortable, searchable, paginated table without dependencies
+                            thanks to simple-datatables</p>
+                    </div>
+                    <div class="col-12 col-md-6 order-md-2 order-first">
+                        <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Pengembalian</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <section class="section">
+                <div class="card">
+                    <div class="card-header">
+                        Data Pengembalian
+                    </div>
+                    <div>
+                        <form action="" method="POST">
 
-          </div>
-          <div class="card-body">
-            <table class="table table-striped" id="table1">
-              <thead>
-                <tr>
-                  <th>No</th>
-                  <th>Nama Mahasiswa</th>
-                  <th>ID RFID</th>
-                  <th>AKSI</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
+                            <div class="p-3">
+                                <input class="col-form-label rounded" style="border-color:blue" type="text" name="ktm">
+                                <input type="submit" class="btn btn-primary" name="cari" value="Cari Data">
+                                <p class="p-2">Otomatis Terisi Saat Tap RFID</p>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="card-body">
+                        <table class="table table-striped" id="table1">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Mahasiswa</th>
+                                    <th>ID RFID</th>
+                                    <th>AKSI</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
                 $no = 1;
                 if (isset($_POST['cari'])) {
                   $ktm = $_POST['ktm'];
@@ -75,36 +77,38 @@ $kon = new Koneksi();
 
 
                 ?>
-                    <tr>
-                      <td><?= $no ?></td>
-                      <td><?= $row['nama_mahasiswa'] ?></td>
-                      <td><?= $row['id_rfid'] ?></td>
-                      <td>
-                        <a href="javascript:void(0)" class="btn btn-primary" onclick="setCookieAndRedirect(<?= $row['id_pinjam'] ?>)">Proses Pengembalian</a>
+                                <tr>
+                                    <td><?= $no ?></td>
+                                    <td><?= $row['nama_mahasiswa'] ?></td>
+                                    <td><?= $row['id_rfid'] ?></td>
+                                    <td>
+                                        <a href="javascript:void(0)" class="btn btn-primary"
+                                            onclick="setCookieAndRedirect(<?= $row['id_pinjam'] ?>)">Proses
+                                            Pengembalian</a>
 
-                        <script>
-                          function setCookieAndRedirect(idPinjam) {
-                            document.cookie = "idPinjam=" + idPinjam;
-                            window.location.href = "proses_pengembalian.php";
-                          }
-                        </script>
+                                        <script>
+                                        function setCookieAndRedirect(idPinjam) {
+                                            document.cookie = "idPinjam=" + idPinjam;
+                                            window.location.href = "proses_pengembalian.php";
+                                        }
+                                        </script>
 
-                      </td>
-                    </tr>
-                <?php $no++;
+                                    </td>
+                                </tr>
+                                <?php $no++;
                   }
                 }
                 ?>
 
-              </tbody>
-            </table>
-          </div>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
 
-    <!-- end-modal-hapus -->
-    <?php include_once 'template/footer.php' ?>
+        <!-- end-modal-hapus -->
+        <?php include_once 'template/footer.php' ?>
 </body>
 
 </html>
