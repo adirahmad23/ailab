ff7e0f7dffac36f2ff3617e7bf16ffac34be1ceb
adirahmad23 <adirahmad607@gmail.com>
2023-07-11 20:47:02 +0700

    Tambahkan Sistem EMAIL peringatan

--- admin/tmpbalik.php
+++ admin/tmpbalik.php
@@ -1,1 +1,1 @@
-<input class='col-form-label rounded' style='border-color:blue' type='text' name='rfid' id='ktmInput' value='130070111C' readonly>
+<input class='col-form-label rounded' style='border-color:blue' type='text' name='rfid' id='ktmInput' value='130070055D' readonly>
