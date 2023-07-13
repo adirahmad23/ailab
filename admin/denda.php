e0dd67f3bfd0ff5e68923cf422531bc90643554e
Chayani Suci <chayanisuci205@gmail.com>
2023-07-13 10:50:07 +0700

    denda

--- admin/denda.php
+++ admin/denda.php
@@ -16,14 +16,14 @@
             <div class="page-title">
                 <div class="row">
                     <div class="col-12 col-md-6 order-md-1 order-last">
-                        <h3>Persetujuan Peminjaman</h3>
+                        <h3>Denda Peminjaman</h3>
 
                     </div>
                     <div class="col-12 col-md-6 order-md-2 order-first">
                         <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Persetujuan Peminjaman</li>
+                                <li class="breadcrumb-item active" aria-current="page">Denda Peminjaman</li>
                             </ol>
                         </nav>
                     </div>
