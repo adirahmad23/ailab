8b92dc8a0dfb297c3a5d464850716dddbb9338ca
adirahmad23 <adirahmad607@gmail.com>
2023-07-26 11:33:45 +0700

    edit bugg peminjaman

--- admin/datamhsw.php
+++ admin/datamhsw.php
@@ -82,7 +82,11 @@ if (isset($_POST['edit'])) {
     <?php include_once "template/sidebar.php" ?>
     <div id="main">
 
-
+        <header class="mb-3">
+            <a href="#" class="burger-btn d-block d-xl-none">
+                <i class="bi bi-justify fs-3"></i>
+            </a>
+        </header>
         <div class="page-heading">
             <div class="page-title">
                 <div class="row">
